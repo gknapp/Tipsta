070077c7db1983a1e661da54ddd94f35d52364cd
Greg K <virtual.greg@gmail.com>
2010-04-10 10:42:01 +0100

    Lint checked, added header comment and minified

--- jquery.tipsta.js
+++ jquery.tipsta.js
@@ -1,211 +1,224 @@
+/**
+ * Tipsta - jQuery plugin to display custom animated tooltips
+ *
+ * @version 1.0 (2010 April 10th)
+ * @require jQuery 1.4 or above
+ * @require jQuery easing functions 1.3+ (for animation effects)
+ *
+ * @author Greg Knapp (virtual dot greg at gmail dot com)
+ * http://www.gregk.co.uk/software/tipsta
+ */
+
 (function($) {
-    $.fn.tipsta = function(options) {  
-        var options = $.extend({}, $.fn.tipsta.defaults, options);
+  $.fn.tipsta = function(options) {  
+    options = $.extend({}, $.fn.tipsta.defaults, options);
         
-		function _sortAsc(a, b) {
-			return (a - b);
-		}
-	
-		function _getNumeric(value) {
-			value = value.replace(/[^-\.0-9]/g, "");
-			
-			if (value == "") {
-				value = 0;
-			}
-			
-			return Math.round(parseFloat(value));
-		}
-		
-		function _isImageMap(elem) {
-			var result = false;
-			
-			if (elem.attr("tagName").toLowerCase() == "map") {
-				result = true;
-			}
-			
-			return result;
-		}
-		        
-        function _createTip(id) {
-			var tt = $("<div>").attr("id", id);
-			
-			if (arguments[1]) { // caption specified
-				tt.html(arguments[1]);
-			}
-		
-			return tt.addClass(options.class).appendTo(options.anchor);			
-		}
-		
-		function _getCaption(elem, cap) {
-			if (cap != undefined && cap != "") { // caption specified
-				return cap;
-			}
-		
-			// try to obtain caption from 'alt' or 'title' attribute
-			var caption = "";
-				
-			if (_isImageMap(elem)) {
-				// use area attributes, if one exists
-				var area = elem.find("area");
-				
-				if (area.length) {
-					// if more than one area, use idx specified
-					elem = (area.length > 1) ? area[options.area] : area;
-				}
-			}
-			
-			var alt = elem.attr("alt");
-			var ttl = elem.attr("title");
-			
-			if (alt != undefined && alt != "") { // use alt
-				caption = alt;
-			} else if (ttl != undefined && alt != "") { // use title
-				caption = ttl;
-			}
-			
-			return caption;
-		}
+    function _sortAsc(a, b) {
+      return a - b;
+    }
+  
+    function _getNumeric(value) {
+      value = value.replace(/[^\-\.0-9]/g, "");
+      
+      if (value == "") {
+        value = 0;
+      }
+      
+      return Math.round(parseFloat(value));
+    }
+    
+    function _isImageMap(elem) {
+      var result = false;
+      
+      if (elem.attr("tagName").toLowerCase() == "map") {
+        result = true;
+      }
+      
+      return result;
+    }
+            
+    function _createTip(id) {
+      var tt = $("<div>").attr("id", id);
+      
+      if (arguments[1]) { // caption specified
+        tt.html(arguments[1]);
+      }
+    
+      if (options.cssClass != "") {
+        tt.addClass(options.cssClass);
+      }
+    
+      return tt.appendTo(options.anchor);      
+    }
+    
+    function _getCaption(elem, cap) {
+      if (cap !== undefined && cap != "") { // caption specified
+        return cap;
+      }
+    
+      // try to obtain caption from 'alt' or 'title' attribute
+      var caption = "";
+        
+      if (_isImageMap(elem)) {
+        // use area attributes, if one exists
+        var area = elem.find("area");
+        
+        if (area.length) {
+          // if more than one area, use idx specified
+          elem = (area.length > 1) ? area[options.area] : area;
+        }
+      }
+      
+      var alt = elem.attr("alt");
+      var ttl = elem.attr("title");
+      
+      if (alt !== undefined && alt != "") { // use alt
+        caption = alt;
+      } else if (ttl !== undefined && alt != "") { // use title
+        caption = ttl;
+      }
+      
+      return caption;
+    }
 
-		function _calcPosition(elem, tip) {
-			var top = 0;
-			var left = 0;
-			var p; // parent element
-		
-			if (_isImageMap(elem)) {
-				var area = elem.find("area");
-					
-				if (area.length) {
-					// if more than one area, use idx specified
-					area = (area.length > 1) ? area[options.area] : area;
-				} else {
-					throw new Error(
-						"No area found for image map '" + elem.attr("id") + "'"
-					);
-				}
-				
-				var coords = area.attr("coords").split(",");
-				var x = [];
-				var y = [];
-				var v;
-				
-				for (var i = 1, j = coords.length; i <= j; i++) {
-					v = parseInt(coords[i-1], 10);
-					
-					if (i / 2 != Math.round(i / 2)) { // odd
-						x[x.length] = v;
-					} else { // even
-						y[y.length] = v;
-					}
-				}
-				
-				x.sort(_sortAsc);
-				y.sort(_sortAsc);
-			
-				// calc dimensions, max - min
-				var mapHeight = y[y.length - 1] - y[0];
-				var mapWidth  = x[x.length - 1] - x[0];
-				
-				top = mapHeight + y[0];
-				left = Math.round(mapWidth / 2) + x[0];
-			
-				// get the image using this map
-				var img = $("img[usemap=\\#" + elem.attr("id") + "]");
-				
-				if (!img.length) {
-					throw new Error(
-						"Could not find an image using map " + elem.attr("id")
-					);
-				}
-				
-				// look for parent to obtain top/left/margin/padding
-				p = $(img).parent();
-			} else {
-				p = elem.parent();
-			}
-	
-			// css attributes that can contribute to positioning		
-			var attrs = ["","margin-","padding-"];
-			x = options.x;
-			y = options.y;
-			
-			// add values to offset
-			for (var i = 0, j = attrs.length; i < j; i++) {
-				y += _getNumeric(p.css(attrs[i] + "top"));
-				x += _getNumeric(p.css(attrs[i] + "left"));
-			}
-			
-			// subtract half the width of tooltip
-			// add the vertical padding and margin on the tip
-			
-			// temporarily set display to block to obtain offsetWidth
-			tip.css({ visibility: "hidden", display: "block" });
-			x -= Math.round(tip.attr("offsetWidth") / 2);
-			tip.css({ visibility: "visible", display: "none" });
-			
-			y += _getNumeric(tip.css("padding-top"));
-			y += _getNumeric(tip.css("margin-top"));
-			
-			top += y;
-			left += x;
-			
-			return { top: top, left: left }
-		}
+    function _calcPosition(elem, tip) {
+      var top = 0;
+      var left = 0;
+      var p; // parent element
+      var i, j;
+    
+      if (_isImageMap(elem)) {
+        var area = elem.find("area");
+          
+        if (area.length) {
+          // if more than one area, use idx specified
+          area = (area.length > 1) ? area[options.area] : area;
+        } else {
+          throw new Error(
+            "No area found for image map '" + elem.attr("id") + "'"
+          );
+        }
+        
+        var coords = area.attr("coords").split(",");
+        var x = [];
+        var y = [];
+        var v;
+        
+        for (i = 1, j = coords.length; i <= j; i++) {
+          v = parseInt(coords[i-1], 10);
+          
+          if (i / 2 != Math.round(i / 2)) { // odd
+            x[x.length] = v;
+          } else { // even
+            y[y.length] = v;
+          }
+        }
+        
+        x.sort(_sortAsc);
+        y.sort(_sortAsc);
+      
+        // calc dimensions, max - min
+        var mapHeight = y[y.length - 1] - y[0];
+        var mapWidth  = x[x.length - 1] - x[0];
+        
+        top = mapHeight + y[0];
+        left = Math.round(mapWidth / 2) + x[0];
+      
+        // get the image using this map
+        var img = $("img[usemap=\\#" + elem.attr("id") + "]");
+        
+        if (!img.length) {
+          throw new Error(
+            "Could not find an image using map " + elem.attr("id")
+          );
+        }
+        
+        // look for parent to obtain top/left/margin/padding
+        p = $(img).parent();
+      } else {
+        p = elem.parent();
+      }
+  
+      // css attributes that can contribute to positioning    
+      var attrs = ["","margin-","padding-"];
+      var offset = { x: options.x, y: options.y };
+      
+      // add values to offset
+      for (i = 0, j = attrs.length; i < j; i++) {
+        offset.y += _getNumeric(p.css(attrs[i] + "top"));
+        offset.x += _getNumeric(p.css(attrs[i] + "left"));
+      }
+      
+      // subtract half the width of tooltip
+      // add the vertical padding and margin on the tip
+      
+      // temporarily set display to block to obtain offsetWidth
+      tip.css({ visibility: "hidden", display: "block" });
+      offset.x -= Math.round(tip.attr("offsetWidth") / 2);
+      tip.css({ visibility: "visible", display: "none" });
+      
+      offset.y += _getNumeric(tip.css("padding-top"));
+      offset.y += _getNumeric(tip.css("margin-top"));
+      
+      top += offset.y;
+      left += offset.x;
+      
+      return { top: top, left: left };
+    }
 
-        return this.each(function() {
-			if ($(this).length == 0) {
-				throw new Error("Cannot find " + elem + " in document");
-			}
-			
-			var id = $(this).attr("id") + "_tip";
-			var tip = $(id);
-			
-			if (!tip.length) {
-				tip = _createTip(
-					id,
-					_getCaption($(this), options.caption)
-				);
-			}
-			
-			var position = _calcPosition($(this), tip);
+    return this.each(function() {
+      if (!$(this).length) {
+        throw new Error("Cannot find " + $(this).attr("id") + " in document");
+      }
+      
+      var id = $(this).attr("id") + "_tip";
+      var tip = $(id);
+      
+      if (!tip.length) {
+        tip = _createTip(
+          id, _getCaption($(this), options.caption)
+        );
+      }
+      
+      var position = _calcPosition($(this), tip);
 
-			tip.css("top", position.top);
-			tip.css("left", position.left);
-			
-			$(this).hover(
-				function(e) {
-					tip.fadeIn(options.duration).animate(
-						{ top: position.top - options.distance },
-						{
-							duration: options.duration,
-							easing: options.easing,
-							queue: false
-						}
-					);
-				},
-				function(e) {
-					tip.animate(
-						{ top: position.top },
-						{
-							duration: options.duration,
-							easing: options.easing,
-							queue: false
-						}
-					).fadeOut(options.duration);
-				}
-			);
-        });
-    };
+      tip.css("top", position.top);
+      tip.css("left", position.left);
+      
+      $(this).hover(
+        function(e) {
+          tip.fadeIn(options.duration).animate(
+            { top: position.top - options.distance },
+            {
+              duration: options.duration,
+              easing: options.easing,
+              queue: false
+            }
+          );
+        },
+        function(e) {
+          tip.animate(
+            { top: position.top },
+            {
+              duration: options.duration,
+              easing: options.easing,
+              queue: false
+            }
+          ).fadeOut(options.duration);
+        }
+      );
+    });
+  };
 })(jQuery);
 
 $.fn.tipsta.defaults = {
-	anchor: "body",
-	area: 0,
-	caption: "",
-	class: "tooltip",
-	distance: 20,
-	duration: 120,
-	easing: "easeOutQuad",
-	x: 0,
-	y: 0
+  anchor: "body",
+  area: 0,
+  caption: "",
+  cssClass: "tooltip",
+  distance: 20,
+  duration: 120,
+  easing: "easeOutQuad",
+  x: 0,
+  y: 0
 };
-
